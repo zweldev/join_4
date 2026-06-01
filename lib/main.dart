959d8ee83c8f13e84fda5ff4a2bdd93dab065fd9
Zwel Htet <zwel@mobiointeractive.com>
2026-06-01 12:12:36 +0630

    [update] optimize launch

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'bloc/game_bloc.dart';
 import 'screens/lobby_screen.dart';
 import 'theme/app_theme.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  GoogleFonts.config.allowRuntimeFetching = false;
   runApp(const Connect4App());
 }
 
